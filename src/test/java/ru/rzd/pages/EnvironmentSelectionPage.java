48c400621964def2c7d71678041ee77e266db770
mefesto <tosha.g@mail.ru>
2025-06-02 17:57:02 +0300

    Добавил новый экран онбординга

---
+++ src/test/java/ru/rzd/pages/EnvironmentSelectionPage.java
@@ -0,0 +1,11 @@
+package ru.rzd.pages;
+
+import io.appium.java_client.AppiumDriver;
+
+public class EnvironmentSelectionPage {
+    private AppiumDriver driver;
+
+    public EnvironmentSelectionPage(AppiumDriver driver){
+        this.driver = driver;
+    }
+}
--- src/test/java/ru/rzd/pages/LanguageSelectionPage.java
+++ src/test/java/ru/rzd/pages/LanguageSelectionPage.java
@@ -3,7 +3,7 @@ package ru.rzd.pages;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 
-public class LanguagePage {
+public class LanguageSelectionPage {
     AppiumDriver driver;
 
     private By title = By.id("ru.rzd.pass.debug:id/title");
@@ -12,23 +12,22 @@ public class LanguagePage {
     private By languageIsChina = By.xpath("//android.widget.TextView[@resource-id=\"ru.rzd.pass.debug:id/name\" and @text=\"简体中文\"]");
     private By buttonNext = By.id("ru.rzd.pass.debug:id/continue_button");
 
+    public LanguageSelectionPage(AppiumDriver driver){
+        this.driver = driver;
+    }
+
     public String getTextTitle(){
         return driver.findElement(title).getText();
     }
 
-    public void tapButtonNext(){
+    public LanguageSelectionPage selectLanguage(String language) {
+        By languageOption = By.xpath("//android.widget.TextView[@resource-id='ru.rzd.pass.debug:id/name' and @text='" + language + "']");
+        driver.findElement(languageOption).click();
+        return this;
+    }
+
+    public EnvironmentSelectionPage tapNext(){
         driver.findElement(buttonNext).click();
-    }
-
-    public void tapLanguageIsRussia(){
-        driver.findElement(languageIsRussia).click();
-    }
-
-    public void tapLanguageIsEnglish(){
-        driver.findElement(languageIsEnglish).click();
-    }
-
-    public void tapLanguageIsChina(){
-        driver.findElement(languageIsChina).click();
+        return new EnvironmentSelectionPage(driver);
     }
 }
