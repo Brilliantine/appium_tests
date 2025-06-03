2aa125577935edc3f6db33d340df695d7a6aa56d
gainitdinovaa <tosha.g@mail.ru>
2025-06-03 11:47:57 +0300

    Добавил в AgreementKasperskyPage метод клика по кнопкам

--- src/test/java/ru/rzd/pages/AgreementKasperskyPage.java
+++ src/test/java/ru/rzd/pages/AgreementKasperskyPage.java
@@ -1,11 +1,30 @@
 package ru.rzd.pages;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
 
 public class AgreementKasperskyPage {
     private AppiumDriver driver;
 
     public AgreementKasperskyPage(AppiumDriver driver){
         this.driver = driver;
+    }
+
+    public MainPage clickButton(String agree){
+        By button;
+        switch (agree){
+            case "agree":
+                button = By.id("ru.rzd.pass.debug:id/accept_button");
+                break;
+
+            case "disagree":
+                button = By.id("ru.rzd.pass.debug:id/cancel_button");
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown button ID: " + agree);
+        }
+        driver.findElement(button).click();
+        return new MainPage(driver);
     }
 }
