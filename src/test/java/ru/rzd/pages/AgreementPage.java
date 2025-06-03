cb7bbc82dbd8a66ba28cb789216a63b9f2356dac
mefesto <tosha.g@mail.ru>
2025-06-03 10:36:08 +0300

    Добавил скролл в EnvironmentSelectionPage

---
+++ src/test/java/ru/rzd/pages/AgreementPage.java
@@ -0,0 +1,11 @@
+package ru.rzd.pages;
+
+import io.appium.java_client.AppiumDriver;
+
+public class AgreementPage {
+    private AppiumDriver driver;
+
+    public AgreementPage(AppiumDriver driver){
+        this.driver = driver;
+    }
+}
