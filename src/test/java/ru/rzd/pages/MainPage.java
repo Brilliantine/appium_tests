82d6f1b14be68fab5cb4659831d9cccdf06c1a27
gainitdinovaa <tosha.g@mail.ru>
2025-06-04 16:00:40 +0300

    Небольшие правки 2

--- src/test/java/ru/rzd/pages/MainPage.java
+++ src/test/java/ru/rzd/pages/MainPage.java
@@ -3,7 +3,7 @@ package ru.rzd.pages;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 
-public class MainPage {
+public class MainPage extends BasePage{
     private AppiumDriver driver;
     //Локаторы
     private By buttonLogin = By.id("ru.rzd.pass.debug:id/sign_in_text_view");
@@ -12,12 +12,13 @@ public class MainPage {
     private By buttonSearchTrains = By.id("ru.rzd.pass.debug:id/search_button");
 
     public MainPage(AppiumDriver driver){
-        this.driver = driver;
+        super(driver);
     }
 
     //Нажатие на кнопку "Войти"
     public LoginPage tapButtonLogin(){
-        driver.findElement(buttonLogin).click();
+        waitAndClick(buttonLogin);
+        //driver.findElement(buttonLogin).click();
         return new LoginPage(driver);
     }
 
