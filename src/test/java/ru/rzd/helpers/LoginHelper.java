e910cde3afee811ea9570d1cbed4b13136a34255
mefesto <tosha.g@mail.ru>
2025-06-05 12:47:04 +0300

    Вынес метод авторизации в отдельный вспомогательный класс

---
+++ src/test/java/ru/rzd/helpers/LoginHelper.java
@@ -0,0 +1,21 @@
+package ru.rzd.helpers;
+
+import io.appium.java_client.AppiumDriver;
+import ru.rzd.pages.LoginPage;
+import ru.rzd.pages.MainPage;
+
+public class LoginHelper {
+    private AppiumDriver driver;
+
+    public LoginHelper(AppiumDriver driver){
+        this.driver = driver;
+    }
+    //Авторизация в приложении
+    public MainPage loginAs(String username, String password){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterLogin(username)
+                .enterPassword(password)
+                .tapButtonLogin();
+        return new MainPage(driver);
+    }
+}
--- src/test/java/ru/rzd/tests/LoginTest.java
+++ src/test/java/ru/rzd/tests/LoginTest.java
@@ -2,6 +2,7 @@ package ru.rzd.tests;
 
 import org.testng.annotations.Test;
 import ru.rzd.base.BaseTest;
+import ru.rzd.helpers.LoginHelper;
 import ru.rzd.pages.LoginPage;
 import ru.rzd.pages.MainPage;
 
