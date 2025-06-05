a71b60c30d0260225dd0b3ebbb0e0b4f570c1e89
mefesto <tosha.g@mail.ru>
2025-06-05 09:10:57 +0300

    Отредачил MainPage

--- src/test/java/ru/rzd/pages/MainPage.java
+++ src/test/java/ru/rzd/pages/MainPage.java
@@ -4,7 +4,6 @@ import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 
 public class MainPage extends BasePage{
-    private AppiumDriver driver;
     //Локаторы
     private By buttonLogin = By.id("ru.rzd.pass.debug:id/sign_in_text_view");
     private By buttonSideMenu = By.id("ru.rzd.pass.debug:id/sign_in_text_view");
@@ -14,20 +13,19 @@ public class MainPage extends BasePage{
     public MainPage(AppiumDriver driver){
         super(driver);
     }
-
+    //Проверяем, что находимся на главной странице
+    public boolean isPageDisplayed(){
+        return isElementPresent(buttonSearchTrains);
+    }
     //Нажатие на кнопку "Войти"
     public LoginPage tapButtonLogin(){
         waitAndClick(buttonLogin);
-        //driver.findElement(buttonLogin).click();
         return new LoginPage(driver);
     }
-
     //Вызов бокового меню
-    public void tapButtonSideMenu(){driver.findElement(buttonSideMenu).click();}
-
+    public void tapButtonSideMenu(){waitAndClick(buttonSideMenu);}
     //Нажатие на кнопку Корзина
-    public void tapButtonBasket(){driver.findElement(buttonBsket).click();}
-
+    public void tapButtonBasket(){waitAndClick(buttonBsket);}
     //Нажатие на кнопку поиска расписания
-    public void tapButtonSearchTrains(){driver.findElement(buttonSearchTrains).click();}
+    public void tapButtonSearchTrains(){waitAndClick(buttonSearchTrains);}
 }
