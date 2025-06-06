2ac9eccbe1b37e442590bd925f6243cab3099a26
Brilliantine <tosha.g@mail.ru>
2025-06-06 12:20:29 +0300

    Начал реализовывать AccesCodePage

---
+++ src/test/java/ru/rzd/pages/AccesCodePage.java
@@ -0,0 +1,15 @@
+package ru.rzd.pages;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+
+public class AccesCodePage extends BasePage{
+
+    private By hint = By.id("ru.rzd.pass.debug:id/hint");
+    private By buttonCanel = By.id("ru.rzd.pass.debug:id/btn_cancel");
+    private By buttonBackspace = By.id("ru.rzd.pass.debug:id/btn_backspace");
+
+    public AccesCodePage(AppiumDriver driver){
+        super(driver);
+    }
+}
