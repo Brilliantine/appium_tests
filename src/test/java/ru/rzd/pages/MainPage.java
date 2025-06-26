788a04c774b9dea6e1340eb6e1f461e6cb398d80
gainitdinovaa <tosha.g@mail.ru>
2025-06-26 15:41:41 +0300

    Добавил Step в MainPage

--- src/test/java/ru/rzd/pages/MainPage.java
+++ src/test/java/ru/rzd/pages/MainPage.java
@@ -1,6 +1,7 @@
 package ru.rzd.pages;
 
 import io.appium.java_client.AppiumDriver;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 public class MainPage extends BasePage{
@@ -15,25 +16,33 @@ public class MainPage extends BasePage{
         super(driver);
     }
     //Проверяем, что находимся на главной странице
+    @Step("Проверяем что находимся на главном экране")
     public boolean isPageDisplayed(){
         return isElementPresent(buttonSearchTrains);
     }
     //Нажатие на кнопку "Войти"
+    @Step("Нажимаем на кнопку войти: {buttonLogin}")
     public LoginPage tapButtonLogin(){
         waitAndClick(buttonLogin);
         return new LoginPage(driver);
     }
     //Вызов бокового меню
+    @Step("Нажимаем на кнопку вызова бокового меню: {buttonSideMenu}")
     public void tapButtonSideMenu(){waitAndClick(buttonSideMenu);}
+
     //Нажатие на кнопку Корзина
+    @Step("Нажимаем на кнопку Корзина: {buttonBsket}")
     public void tapButtonBasket(){waitAndClick(buttonBsket);}
     //Нажатие на кнопку поиска расписания
+    @Step("Нажимаем на кнопку поиска расписания: {buttonSearchTrains}")
     public void tapButtonSearchTrains(){waitAndClick(buttonSearchTrains);}
     //Проверяем, отображается ли аватар пользователя
+    @Step("Проверяем отображается ли аватар")
     public boolean isUserAvatarVisible(){
         return isElementPresent(avatarImage);
     }
     //Проверяем, отображается ли кнопка Войти
+    @Step("Нажимаем на кнопку Войти: {buttonLogin}")
     public boolean isLoginButtonVisible(){
         return isElementPresent(buttonLogin);
     }
